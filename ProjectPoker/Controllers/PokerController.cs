22ed0f634285f14ba90fc8269d792be7c19596a7
DonovanDeSmedt <donovandesmedt@live.be>
2016-01-11 11:19:50 +0100

    Update PokerController

--- ProjectPoker/Controllers/PokerController.cs
+++ ProjectPoker/Controllers/PokerController.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Helpers;
 using System.Web.Mvc;
-using Newtonsoft.Json;
 using ProjectPoker.Models;
 using ProjectPoker.ViewModels;
 
